b42d8a50a25bbf3251091aad3b58cd3129173d8b
Wael Almattar <waelsy123@gmail.com>
2025-08-06 11:48:34 +0200

    feat: add `_calculateProtocolFee` to PaymentLogic module

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -3,5 +3,5 @@ pragma solidity 0.8.28;
 
 address constant ZERO_ADDRESS = address(0);
 
-uint256 constant PROTOCOL_FEE_PRECISION = 1e6;
-uint256 constant PROTOCOL_FEE = 9_000; // 0.9% fee with 6 decimals precision
+uint256 constant PROTOCOL_FEE_PRECISION = 1e4;
+uint256 constant PROTOCOL_FEE = 90; // 0.9% fee with 4 decimals precision

5b68fc83b2eea8225e67939d85114c1a242afbc2
Wael Almattar <waelsy123@gmail.com>
2025-08-06 13:31:57 +0200

    chore: package contract storage into struct

--- test/mocks/ZKPayV2.sol
+++ test/mocks/ZKPayV2.sol
@@ -1,13 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {ZKPayStorage} from "../../src/ZKPayStorage.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {AssetManagement} from "../../src/libraries/AssetManagement.sol";
+import {MerchantLogic} from "../../src/libraries/MerchantLogic.sol";
+import {SwapLogic} from "../../src/libraries/SwapLogic.sol";
+import {PayWallLogic} from "../../src/libraries/PayWallLogic.sol";
+import {EscrowPayment} from "../../src/libraries/EscrowPayment.sol";
 
 /// @custom:oz-upgrades-from ZKPay
-contract ZKPayV2 is ZKPayStorage, Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract ZKPayV2 is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    // solhint-disable-next-line gas-struct-packing
+    struct ZKPayStorage {
+        address sxt;
+        address treasury;
+        address executorAddress;
+        mapping(address asset => AssetManagement.PaymentAsset) assets;
+        mapping(address merchantAddress => MerchantLogic.MerchantConfig merchantConfig) merchantConfigs;
+        SwapLogic.SwapLogicStorage swapLogicStorage;
+        PayWallLogic.PayWallLogicStorage paywallLogicStorage;
+        EscrowPayment.EscrowPaymentStorage escrowPaymentStorage;
+    }
+
+    ZKPayStorage internal _zkPayStorage;
     uint256 public constant VERSION = 2;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
